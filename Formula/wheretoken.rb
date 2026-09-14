993fc5b81b7264478154a7c37ab3e0c2109599a8
rainhuang0220 <iamrainhuang@163.com>
2026-09-14 19:20:30 +0800

    chore: update wheretoken to v0.7.3

--- Formula/wheretoken.rb
+++ Formula/wheretoken.rb
@@ -1,28 +1,28 @@
 class Wheretoken < Formula
   desc "Local coding-agent token usage as a character table"
   homepage "https://github.com/rainhuang0220/whereToken"
-  version "0.7.2"
+  version "0.7.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/rainhuang0220/whereToken/releases/download/v0.7.2/wheretoken_darwin_arm64.tar.gz"
-      sha256 "41e33c43a23f6862d00aa64fa53a2c3e3a7a58d48b7e66d52ee10de72aa0e3e9"
+      url "https://github.com/rainhuang0220/whereToken/releases/download/v0.7.3/wheretoken_darwin_arm64.tar.gz"
+      sha256 "fd4966831b8d6ee72d912934f15047cb7a47e3733832b7896f32b5ead9598e55"
     end
     on_intel do
-      url "https://github.com/rainhuang0220/whereToken/releases/download/v0.7.2/wheretoken_darwin_amd64.tar.gz"
-      sha256 "214f26ca8d130a0605910cf36a994588e5239be7d5adf68bafc6df838af25836"
+      url "https://github.com/rainhuang0220/whereToken/releases/download/v0.7.3/wheretoken_darwin_amd64.tar.gz"
+      sha256 "d6ce18bc2caf1947d45f84a533fa66bf28bfa9479c6e13817cd1093a2ce97069"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/rainhuang0220/whereToken/releases/download/v0.7.2/wheretoken_linux_arm64.tar.gz"
-      sha256 "ea5f5e5961dc3ef518b5e3a114aafd7dc269c10ea4e2fed75ed13875eedf9853"
+      url "https://github.com/rainhuang0220/whereToken/releases/download/v0.7.3/wheretoken_linux_arm64.tar.gz"
+      sha256 "64710f574cafd55a1e3a88276f1f0317a99887f09f1ded47e07d4f7f93384355"
     end
     on_intel do
-      url "https://github.com/rainhuang0220/whereToken/releases/download/v0.7.2/wheretoken_linux_amd64.tar.gz"
-      sha256 "12767bce30ce66ce80cf6ed847d7d6d54c8b48bd1f487f0584340cb5f75f4aa6"
+      url "https://github.com/rainhuang0220/whereToken/releases/download/v0.7.3/wheretoken_linux_amd64.tar.gz"
+      sha256 "75f14f501b542030a112b85f15af153701bac8a2dc05bc34a7c451c827ea29bd"
     end
   end
 
